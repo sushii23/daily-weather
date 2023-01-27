c282b8db6d97f18f89c61d14f4a1deb025bafab8
Adrian Deon <sushdawg@Adrians-MBP.lan1>
2023-01-27 07:00:48 -0600

    added script.js

--- main/script.js
+++ main/script.js
@@ -9,6 +9,7 @@ const date = document.getElementById("date");
 const temperature = document.getElementById("temperature");
 const humidity = document.getElementById("humidity");
 const windspeed = document.getElementById("wind-speed");
+const weatherIcon = document.getElementById("weather-icon");
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -41,3 +42,30 @@ function getFiveDayForecast(city) {
   fetch(
     `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
   )
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    forecast.innerHTML = "";
+
+    for (let i = 0; i < data.list.length; i++) {
+      if (data.list[i].dt_txt.includes("12:00:00")) {
+        const forecastDate = data.list[i].dt_txt;
+        const forecastIcon = data.list[i].weather[0].icon;
+        const forecastTemperature = data.list[i].main.temp;
+        const forecastWindSpeed = data.list[i].wind.speed;
+        const forecastHumidity = data.list[i].main.humidity;
+
+        forecast.innerHTML += `
+        <div class="forecast-day">
+          <p class="forecast-date">${forecastDate}</p>
+          <img src="http://openweathermap.org/img/wn/${forecastIcon}
+          <p class="forecast-temperature">Temperature: ${forecastTemperature} F</p>
+          <p class="forecast-wind-speed">Wind Speed: ${forecastWindSpeed} mph</p>
+          <p class="forecast-humidity">Humidity: ${forecastHumidity}%</p>
+        </div>
+      `;
+    }
+  }
+})
+  .catch((error) => console.log (error));
+} 
