ae0f79c8d3163b4498acd1d8d329f07e028d45e3
Adrian Deon <sushdawg@Adrians-MBP.lan1>
2023-01-27 06:50:12 -0600

    added get weather function with api

--- main/script.js
+++ main/script.js
@@ -19,3 +19,20 @@ searchForm.addEventListener("submit", (e) => {
   getFiveDayForecast(city);
 });
 
+function getCurrentWeather(city) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+  )
+
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    cityName.innerText = data.name;
+    date.innerText = new Date().toLocaleDateString();
+    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    temperature.innerText = `Temperature: ${data.main.temp} F`;
+    humidity.innerText = `Humidity: ${data.main.humidity}%`;
+    windSpeed.innerText = `Wind Speed: ${data.wind.speed} mph`;
+  })
+  .catch((error) => console.log(error));
+}
